f568b92fc56ef6a0bc64c063cf714cf2c19f0812
Zou <xzou9@MGC12Z81MYJV40.fbpld77.ford.com>
2021-02-13 19:13:50 -0500

    working whiteboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
-
-const ENDPOINT = "http://localhost:3001";
+import Container from './components/container/Container';
 
 function App() {
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("connection", data => {
-      setResponse(data);
-    });
-
-    socket.on("test", data => {
-      setResponse(data);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {response}
-        </a>
-      </header>
-    </div>
+      <Container/>
   );
 }
 
 export default App;
-
